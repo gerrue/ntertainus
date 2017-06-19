94e1dd91061b67dde8723a32117cd52ef9a77a28
GERUE-HOME\Thomas <tschmidt78@gmail.com>
2017-06-18 23:26:28 -0400

    Add Home View

---
+++ src/views/AppHome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <app-popular></app-popular>
+    <app-upcoming></app-upcoming>
+  </div>
+</template>
+
+<script>
+import AppUpcoming from '../components/AppUpcoming';
+import AppPopular from '../components/AppPopular';
+export default {
+  name: 'appHome',
+  components: {
+    AppUpcoming,
+    AppPopular,
+  }
+}
+</script>
+
+<style>
+</style>
